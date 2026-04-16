a11aa7def942c1a06607be3a05d02cffc2823947
Mouhamad-A <mouhamad69140@gmail.com>
2026-04-16 11:10:39 +0200

    commit 2

---
+++ src/netlify/functions/commandes.js
@@ -0,0 +1,16 @@
+exports.handler = async function (event) {
+    const { nom, produit } = JSON.parse(event.body)
+
+    await fetch(process.env.DISCORD_WEBHOOK, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            content: `Nouvelle commande !\n**Nom :** ${nom}\n**Produit :** ${produit}`
+        })
+    })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true })
+    }
+}
